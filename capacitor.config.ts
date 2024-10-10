a558fabd54c5ca97d63bd217ea318ef3d4233e45
bvaldenegro <bra.valdenegro@duocuc.cl>
2024-10-10 17:40:28 -0300

    Actualización del archivo capacitor.config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Uber',
+  appId: 'cl.valdevergara.HyperLinkRide',
+  appName: 'Hyper LinkRide',
   webDir: 'www'
 };
 
