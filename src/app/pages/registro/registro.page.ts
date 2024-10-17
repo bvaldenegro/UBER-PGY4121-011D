aae2d95e8529adc2617f7527fd8ba5defe073a15
bvaldenegro <bra.valdenegro@duocuc.cl>
2024-10-16 23:35:37 -0300

    Añadiendo la función para agregar imagenes al registro

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { HelperService } from 'src/app/services/helper.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-registro',
@@ -16,8 +17,9 @@ export class RegistroPage implements OnInit {
   telefono:string ="";
   password1:string = "";
   password2:string = "";
+  imagen:any;
 
-  constructor(private router:Router, private firebase:FirebaseService, private usuario:UsuarioService, private helper:HelperService) { }
+  constructor(private router:Router, private firebase:FirebaseService, private usuarioService:UsuarioService, private helper:HelperService) { }
 
   ngOnInit() {
   }
@@ -40,8 +42,28 @@ export class RegistroPage implements OnInit {
     }*/
       const loader = await this.helper.showLoader("Cargando...");
       try {
-        await this.firebase.registro(this.correo, this.password1);
-        loader.dismiss();
+        // await this.firebase.registro(this.correo, this.password1);
+
+        const userFirebase = await this.firebase.registro(this.correo, this.password1);
+        const token = await userFirebase.user?.getIdToken();
+
+        if(token){
+          const req = await this.usuarioService.agregarUsuario(
+            {
+              p_correo_electronico:this.correo,
+              p_nombre:this.nombre,
+              p_telefono:this.telefono,
+              token:token
+            },
+            this.imagen
+          )
+        }
+
+        await this.helper.showAlert("Usuario creado exitosamente", "Informacion");
+        await this.router.navigateByUrl('login');
+
+        await loader.dismiss();
+
       } catch (error:any) {
         let msg = "Error al registrar usuario"
         if (error.code == "auth/email-already-in-use") {
@@ -55,4 +77,25 @@ export class RegistroPage implements OnInit {
         loader.dismiss();
       }
   }
+
+  async takePhoto() {
+    const image = await Camera.getPhoto({
+      quality : 90,
+      allowEditing : true,
+      resultType : CameraResultType.Uri
+    });
+    if(image.webPath){
+      const response = await fetch(image.webPath);
+      const blob = await response.blob();
+
+      this.imagen = {
+        fname: 'foto' + image.format,
+        src: image.webPath,
+        file : blob
+      }
+      var imageUrl = image.webPath;
+      this.imagen.src = imageUrl;
+    }
+  }
+
 }
