2b4bdf5d8552f082a06b042b8329897c28b6ef58
bvaldenegro <bra.valdenegro@duocuc.cl>
2024-10-22 07:37:46 -0300

    Agregando cargar usuario a el perfil

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserModel } from 'src/app/models/usuario';
+import { StorageService } from 'src/app/services/storage.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-perfil',
@@ -8,12 +11,29 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class PerfilPage implements OnInit {
 
-  usuario:string = "";
+  usuario:UserModel[] = [];
 
-  constructor(private activatedRoute:ActivatedRoute, private router:Router) { }
+  constructor(private activatedRoute:ActivatedRoute, private router:Router, private storage:StorageService,
+              private usuarioService: UsuarioService
+  ) { }
+
+
+  async cargarUsuario(){
+    let dataStorage = await this.storage.obtenerStorage();
+
+    const req = await this.usuarioService.obtenerUsuario(
+      {
+        p_correo: dataStorage[0].usuario_correo,
+        token: dataStorage[0].token
+      }
+    );
+    this.usuario = req.data;
+    console.log("Usuario cargado: ", this.usuario)
+  }
 
   ngOnInit() {
     
+    this.cargarUsuario();
     this.usuario = this.activatedRoute.snapshot.params["usuario"];
 
   }
