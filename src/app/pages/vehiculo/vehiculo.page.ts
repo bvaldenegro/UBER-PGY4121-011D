07528f0aca06fbedcba81db57f4c356b029eb135
bvaldenegro <bra.valdenegro@duocuc.cl>
2024-10-22 09:08:48 -0300

    Metodo para agregar vehiculos

--- src/app/pages/vehiculo/vehiculo.page.ts
+++ src/app/pages/vehiculo/vehiculo.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/app/models/usuario';
+import { VehiculoModel } from 'src/app/models/vehiculo';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { HelperService } from 'src/app/services/helper.service';
+import { StorageService } from 'src/app/services/storage.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { VehiculoService } from 'src/app/services/vehiculo.service';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-vehiculo',
@@ -7,22 +15,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehiculoPage implements OnInit {
 
-  vehiculo = {
+/*   vehiculo = {
     marca: '',
     modelo: '',
     ano: '',
     patente: ''
-  };
+  }; */
 
-  vehiculos: any[] = [];
+  patente:string = "";
+  marca:string= "";
+  modelo:string = "";
+  anio:number = 0;
+  color:string = "";
+  tipo_combustible:string = "";
+  nombre_proyecto:string ="";
+  imagen: any = "";
+  usuario:UserModel[] = [];
+  vehiculo:VehiculoModel[] = [];
 
-  constructor() { }
+  constructor(private storage:StorageService, private usuarioService:UsuarioService, private vehiculoService: VehiculoService,
+              private helper:HelperService, private firebase:FirebaseService
+  ) { }
 
   ngOnInit() {
+    this.cargarUsuario();
+    this.cargarVehiculo()
   }
 
-  agregarVehiculo() {
-    // Validamos que todos los campos estén completos
+
+  async cargarUsuario(){
+    let dataStorage = await this.storage.obtenerStorage();
+
+    const req = await this.usuarioService.obtenerUsuario(
+      {
+        p_correo: dataStorage[0].usuario_correo,
+        token: dataStorage[0].token
+      }
+    );
+    this.usuario = req.data;
+    console.log("Usuario cargado: ", this.usuario)
+  }
+
+  async agregarVehiculo() {
+    /* // Validamos que todos los campos estén completos
     if (this.vehiculo.marca && this.vehiculo.modelo && this.vehiculo.ano && this.vehiculo.patente) {
       // Agregamos el vehículo a la lista
       this.vehiculos.push({ ...this.vehiculo });
@@ -30,10 +65,72 @@ export class VehiculoPage implements OnInit {
       this.limpiarFormulario();
     }else{
       alert("Uno o más campos vacios")
+    } */
+    let dataStorage = await this.storage.obtenerStorage()
+    const loader = await this.helper.showLoader('Cargando...')
+    const token = dataStorage[0].token
+    try {
+      if(token){
+        let usuario = this.cargarUsuario()
+        const req = await this.vehiculoService.agregarVehiculo(
+          {
+            p_id_usuario:this.usuario[0].id_usuario,
+            p_patente: this.patente,
+            p_marca: this.marca,
+            p_modelo: this.modelo,
+            p_anio: this.anio,
+            p_color: this.color,
+            p_tipo_combustible: this.tipo_combustible,
+            p_nombre_proyecto: 'valdevergara',
+            token:token
+          }, this.imagen
+        )
+        console.log('Ya po papito xd')
+      }
+      await this.helper.showAlert("vehiculo creado exitosamente", "Informacion");
+      await loader.dismiss();
+
+    } catch (error) {
+      await loader.dismiss()
+      console.log('Error po weon')
+      throw error
+    }
+    
+
+
+  }
+
+
+  async cargarVehiculo(){
+    let dataStorage = await this.storage.obtenerStorage();
+
+    const req = await this.vehiculoService.obtenerVehiculo(dataStorage[0].token)
+    this.vehiculo = req.data;
+  }
+
+  async takePhoto() {
+    const image = await Camera.getPhoto({
+      quality : 90,
+      allowEditing : true,
+      resultType : CameraResultType.Uri
+    });
+    if(image.webPath){
+      const response = await fetch(image.webPath);
+      const blob = await response.blob();
+
+      this.imagen = {
+        fname: 'foto' + image.format,
+        src: image.webPath,
+        file : blob
+      }
+      var imageUrl = image.webPath;
+      this.imagen.src = imageUrl;
     }
   }
 
-  limpiarFormulario() {
+
+
+  /* limpiarFormulario() {
     // Reseteamos el objeto vehiculo para limpiar el formulario
     this.vehiculo = {
       marca: '',
@@ -41,5 +138,5 @@ export class VehiculoPage implements OnInit {
       ano: '',
       patente: ''
     };
-  }
+  } */
 }
---
+++ src/app/services/vehiculo.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehiculoService {
+
+  constructor(private http:HttpClient) { }
+
+
+  async agregarVehiculo(datosVehiculo:dataBodyVehiculo, imgFileUSer:any){
+    try {
+      
+      const formData = new FormData();
+
+      formData.append('p_id_usuario', datosVehiculo.p_id_usuario.toString())
+      formData.append('p_patente', datosVehiculo.p_patente),
+      formData.append('p_marca', datosVehiculo.p_marca),
+      formData.append('p_modelo', datosVehiculo.p_modelo),
+      formData.append('p_anio', datosVehiculo.p_anio.toString()),
+      formData.append('p_color', datosVehiculo.p_color),
+      formData.append('p_tipo_combustible', datosVehiculo.p_tipo_combustible)
+      formData.append('token', datosVehiculo.token)
+      formData.append('imagen_vehiculo', imgFileUSer.file, imgFileUSer.name)
+
+      const response = await lastValueFrom(this.http.post<any>(environment.apiUrl + 'vehiculo/agregar', formData));
+      return response;
+
+
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async obtenerVehiculo(parToken:string){
+    try {
+      
+      const params = {
+        token:parToken
+      }
+      const response = await lastValueFrom(this.http.get<any>(environment.apiUrl + "vehiculo/obtener", {params}))
+      return response
+
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+interface dataBodyVehiculo{
+  p_id_usuario:number;
+  p_patente: string;
+  p_marca: string;
+  p_modelo: string;
+  p_anio: number;
+  p_color: string;
+  p_tipo_combustible: string;
+  p_nombre_proyecto?: string;
+  token:string;
+}
