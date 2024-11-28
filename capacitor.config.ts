0bfddb876f70ea5847dd37e1f00137849eb0ea66
bvaldenegro <bra.valdenegro@duocuc.cl>
2024-11-28 16:50:42 -0300

    Adición del apk, aab y la key junto al icono personalizado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,15 +4,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'cl.valdevergara.HyperLinkRide',
   appName: 'Hyper LinkRide',
-  webDir: 'src',
-  plugins: {
-    Geolocation: {
-      // Permisos para Android
-      permissions: {
-        android: ['android.permission.ACCESS_COARSE_LOCATION', 'android.permission.ACCESS_FINE_LOCATION']
-      }
-    }
-  }
+  webDir: 'www'
 };
 
 export default config;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDGjRptMe2o7GwLgUjyvpvi61DMI8T3kbI",
+    authDomain: "valdevergara.firebaseapp.com",
+    projectId: "valdevergara",
+    storageBucket: "valdevergara.appspot.com",
+    messagingSenderId: "623259229896",
+    appId: "1:623259229896:web:2f25e32ed95c4f2f820ffd"
+  },
+  apiUrl:"https://uber-nodejs-server-git-d61f89-guillermovillacuratorres-projects.vercel.app/api/"
 };
